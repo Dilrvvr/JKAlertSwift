fdec7357dd1b99d0ba39a9ba05466b8657e27972
albert <jkdev123cool@gmail.com>
2020-11-10 14:17:00 +0800

    add class

--- JKAlertSwift/Classes/Base/JKAlertBaseView.swift
+++ JKAlertSwift/Classes/Base/JKAlertBaseView.swift
@@ -17,23 +17,30 @@ public class JKAlertBaseView: UIView {
     // MARK:
     // MARK: - Public Property
     
+    /**
+     * 是否自动添加基本的view
+     * 包括backgroundView & contentView
+     * 默认YES，子类重写getter
+     */
+    private(set) var autoAddBasicViews: Bool = true
+    
+    /** backgroundView */
     public private(set) lazy var backgroundView: UIView = {
         
         let backgroundView = UIView()
+        
         backgroundView.isUserInteractionEnabled = false
-        self.insertSubview(backgroundView, at: 0)
         
         return backgroundView
     }()
     
+    /** contentView */
     public private(set) lazy var contentView: UIView = {
         
         let contentView = UIView()
-        self.insertSubview(contentView, at: 1)
         
         return contentView
     }()
-    
     
     // MARK:
     // MARK: - Override
@@ -59,8 +66,11 @@ public class JKAlertBaseView: UIView {
     public override func layoutSubviews() {
         super.layoutSubviews()
         
-        backgroundView.frame = bounds
-        contentView.frame = bounds
+        if autoAddBasicViews {
+            
+            backgroundView.frame = bounds
+            contentView.frame = bounds
+        }
     }
     
     // MARK:
@@ -98,14 +108,22 @@ public class JKAlertBaseView: UIView {
     /** 创建UI 交给子类重写 super自动调用该方法 */
     internal func createUI() {
         
+        if autoAddBasicViews {
+            
+            self.insertSubview(backgroundView, at: 0)
+            self.insertSubview(contentView, at: 1)
+        }
     }
     
     /** 布局UI 交给子类重写 super自动调用该方法 */
     internal func layoutUI() {
         
-        JKAlertConstraintManager.addZeroEdgeConstraints(targetView: backgroundView, constraintsView: self)
-        
-        JKAlertConstraintManager.addZeroEdgeConstraints(targetView: contentView, constraintsView: self)
+        if autoAddBasicViews {
+            
+            JKAlertConstraintManager.addZeroEdgeConstraints(targetView: backgroundView, constraintsView: self)
+            
+            JKAlertConstraintManager.addZeroEdgeConstraints(targetView: contentView, constraintsView: self)
+        }
     }
     
     /** 初始化UI数据 交给子类重写 super自动调用该方法 */
---
+++ JKAlertSwift/Classes/View/JKAlertTextContainerView.swift
@@ -0,0 +1,104 @@
+//
+//  JKAlertTextContainerView.swift
+//  JKAlertSwift
+//
+//  Created by albert on 2020/11/9.
+//
+
+import UIKit
+
+class JKAlertTextContainerView: JKAlertBaseView {
+    
+    // MARK:
+    // MARK: - Public Property
+    
+    /** textView */
+    private(set) var textView: JKAlertTextView = JKAlertTextView()
+    
+    // MARK:
+    // MARK: - Public Methods
+    
+    /** 计算frame */
+    public func calculateTextFrame(contentWidth: CGFloat, minHeight: CGFloat, originY: CGFloat) -> CGRect {
+        
+        var rect = textView.calculateTextFrame(maxWidth: contentWidth, minHeight: 0.0, originY: 0.0, superView: self)
+        
+        if rect.size.height < minHeight {
+            
+            rect.size.height = minHeight
+        }
+        
+        rect.origin.y = originY
+        
+        frame = rect
+        
+        return rect
+    }
+    
+    // MARK:
+    // MARK: - Override
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        textView.center = CGPoint(x: frame.width * 0.5, y: frame.height * 0.5)
+    }
+    
+    // MARK:
+    // MARK: - Private Methods
+    
+    
+    
+    // MARK:
+    // MARK: - Private Selector
+    
+    
+    
+    // MARK:
+    // MARK: - Custom Delegates
+    
+    
+    
+    // MARK:
+    // MARK: - Initialization & Build UI
+    
+    /** 初始化自身属性 交给子类重写 super自动调用该方法 */
+    override func initializeProperty() {
+        super.initializeProperty()
+        
+    }
+    
+    /** 构造函数初始化时调用 注意调用super */
+    override func initialization() {
+        super.initialization()
+        
+    }
+    
+    /** 创建UI 交给子类重写 super自动调用该方法 */
+    override func createUI() {
+        super.createUI()
+        
+    }
+    
+    /** 布局UI 交给子类重写 super自动调用该方法 */
+    override func layoutUI() {
+        super.layoutUI()
+        
+    }
+    
+    /** 初始化UI数据 交给子类重写 super自动调用该方法 */
+    override func initializeUIData() {
+        super.initializeUIData()
+        
+    }
+    
+    // MARK:
+    // MARK: - Private Property
+    
+    override var autoAddBasicViews: Bool {
+        get {
+            
+            return false
+        }
+    }
+}
